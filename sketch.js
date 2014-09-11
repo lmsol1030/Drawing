33de092fe6e58fd1246a3d98f6204be9646f1a49
lmsol1030 <lmsol1030@naver.com>
2014-09-11 14:19:46 +0900

    1234

--- sketch.js
+++ sketch.js
@@ -7,10 +7,16 @@ function setup() {
   background(255, 176, 176);
   
 
-  stroke(255);
   noFill();
 
+  stroke(255, 56, 56);
   line(10, 10, 90, 10);
+   
+
+   stroke(255, 89, 89);
     rect(20, 20, 80, 50);
+    
+
+    stroke(255, 40, 40);
     ellipse(50, 50, 50, 50);
 }
